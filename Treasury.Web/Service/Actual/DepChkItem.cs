c9fe2d109b3537d85ea173ddf1cf9d11330ab22d
mark <mark.ch.chang@fubon.com>
2018-11-08 18:37:52 +0800

    commit fixed

--- Treasury.Web/Service/Actual/DepChkItem.cs
+++ Treasury.Web/Service/Actual/DepChkItem.cs
@@ -92,13 +92,15 @@ namespace Treasury.Web.Service.Actual
         /// 金庫進出管理作業-駁回
         /// </summary>
         /// <param name="db">Entities</param>
-        /// <param name="itemIDs">駁回的申請單號</param>
+        /// <param name="aplyNos">駁回的申請單號</param>
         /// <param name="logStr">log</param>
         /// <param name="dt">執行時間</param>
+        /// <param name="userId">覆核人ID</param>
+        /// <param name="desc">覆核意見</param>
         /// <returns></returns>
-        public Tuple<bool, string> TinReject(TreasuryDBEntities db, List<string> itemIDs, string logStr, DateTime dt)
+        public Tuple<bool, string> TinReject(TreasuryDBEntities db, List<string> aplyNos, string logStr, DateTime dt, string userId, string desc)
         {
-            foreach (var itemID in itemIDs)
+            foreach (var itemID in aplyNos)
             {
 
             }
@@ -109,13 +111,14 @@ namespace Treasury.Web.Service.Actual
         /// 金庫進出管理作業-覆核
         /// </summary>
         /// <param name="db">Entities</param>
-        /// <param name="itemIDs">覆核的申請單號</param>
+        /// <param name="aplyNos">覆核的申請單號</param>
         /// <param name="logStr">log</param>
         /// <param name="dt">執行時間</param>
+        /// <param name="userId">覆核人ID</param>
         /// <returns></returns>
-        public Tuple<bool, string> TinApproved(TreasuryDBEntities db, List<string> itemIDs, string logStr, DateTime dt)
+        public Tuple<bool, string> TinApproved(TreasuryDBEntities db, List<string> aplyNos, string logStr, DateTime dt, string userId)
         {
-            foreach (var itemID in itemIDs)
+            foreach (var itemID in aplyNos)
             {
 
             }
